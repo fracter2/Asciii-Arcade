28b3dd04301e6f2c76fd3e20caafac6ec215d544
fracter2 <78301601+fracter2@users.noreply.github.com>
2021-11-29 00:05:41 +0100

    Start Stuff, Basic Render with customizable window elements

--- Asciii Arcade/Program.cs
+++ Asciii Arcade/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
+using System.Drawing;
+using System.Windows;
+
 
 namespace Asciii_Arcade
 {
@@ -10,21 +16,65 @@ namespace Asciii_Arcade
     {
         static void Main(string[] args)
         {
-            char[] screenRow = new char[32];
-            List<char[]> screen = new List<char[]>();
-            for (int i = 0; i < 32; i++)
-            {
-                for (int i1 = 0; i < 32; i1++)
-                {
-
-                }
-            }
-
-            for (int i = 0; i < 32; i++)
-            {
-
-            }
+            // Window Settings
+            Console.Title = "Asciii Arcade";
+            Console.SetWindowSize(120, 52);
             
+
+
+            int scrnSizeY = 48;
+            double scrnProp = 2;
+            bool ratioMode = true;
+            int scrnSizeX = (int) (ratioMode ? Math.Ceiling(scrnSizeY * scrnProp) : 40); // Go by Y to X ratio, or designated value
+            
+
+            char[] screenRow = new char[scrnSizeX];
+            for (int i = 0; i < scrnSizeX; i++)
+            {
+                screenRow[i] = ' '; // What to use as blank
+            }
+
+                List<char[]> screen = new List<char[]>(scrnSizeY);
+            for (int i = 0; i < scrnSizeY; i++)
+            {
+                screen.Add(screenRow);
+            }
+
+            void Render()
+            {
+                // Border Top
+                for (int i = 0; i < scrnSizeX + 2; i++) 
+                {
+                    Console.Write("_");
+                }
+                Console.Write("\n");
+
+                for (int i = 0; i < scrnSizeY; i++)
+                {
+                    Console.Write("|"); // Border Left
+                    for (int i1 = 0; i1 < scrnSizeX; i1++)
+                    {
+                        Console.Write(screen[i][i1]);
+                    }
+                    Console.Write("|\n"); // Border Riddle and next line
+                }
+
+                // Border Bottom
+                for (int i = 0; i < scrnSizeX + 2; i++)
+                {
+                    Console.Write("_");
+                }
+                Console.Write("\n");
+            }
+
+
+
+            
+
+            Render();
+            Console.ReadKey();
+
+
         }
     }
 }
