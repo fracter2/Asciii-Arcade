d6dc25ff12db47b7e9a3e535c5c1ea9ed86ec962
fracter2 <78301601+fracter2@users.noreply.github.com>
2021-11-28 17:03:53 +0100

    Add project files.

---
+++ Asciii Arcade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asciii_Arcade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] screenRow = new char[32];
+            List<char[]> screen = new List<char[]>();
+            for (int i = 0; i < 32; i++)
+            {
+                for (int i1 = 0; i < 32; i1++)
+                {
+
+                }
+            }
+
+            for (int i = 0; i < 32; i++)
+            {
+
+            }
+            
+        }
+    }
+}
