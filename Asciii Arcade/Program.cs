c8cd37e8f9f86209644addf5f32bf082f1b54b73
fracter2 <78301601+fracter2@users.noreply.github.com>
2021-11-30 21:04:10 +0100

    Did alot, added classes for the snake, and a basic physics updater.

--- Asciii Arcade/Program.cs
+++ Asciii Arcade/Program.cs
@@ -20,27 +20,26 @@ namespace Asciii_Arcade
             Console.Title = "Asciii Arcade";
             Console.SetWindowSize(120, 52);
             
-
-
             int scrnSizeY = 48;
             double scrnProp = 2;
             bool ratioMode = true;
             int scrnSizeX = (int) (ratioMode ? Math.Ceiling(scrnSizeY * scrnProp) : 40); // Go by Y to X ratio, or designated value
             
 
-            char[] screenRow = new char[scrnSizeX];
+            char[] screenRow = new char[scrnSizeX]; // Basic empty row gen
             for (int i = 0; i < scrnSizeX; i++)
             {
                 screenRow[i] = ' '; // What to use as blank
             }
 
-                List<char[]> screen = new List<char[]>(scrnSizeY);
+                List<char[]> screen = new List<char[]>(scrnSizeY); // basic empty screen maker
             for (int i = 0; i < scrnSizeY; i++)
             {
                 screen.Add(screenRow);
             }
 
-            void Render()
+
+            void Render() // Renders what is inside the screen with border
             {
                 // Border Top
                 for (int i = 0; i < scrnSizeX + 2; i++) 
@@ -67,14 +66,78 @@ namespace Asciii_Arcade
                 Console.Write("\n");
             }
 
+            // void menu () {}
+
+            void SnakeGame()
+            {
+                
+                List<SnakeSegment> snakeBody = new List<SnakeSegment>();
+                SnakeSegment snakeSegBase = new SnakeSegment(0, 0);
+                int velX = 1;
+                int velY = 0;
+
+                snakeBody.Add(snakeSegBase); // Set the start body
+                snakeBody.Add(snakeSegBase);
+                snakeBody.Add(snakeSegBase);
 
 
+                void physicsStep()
+                {
+                    // Heart Loop
+                    // Player Input
+
+                    snakeBody[0].posX += velX;
+                    snakeBody[0].posY += velY;
+
+                    for (int i = 1; i >= snakeBody.Count(); i--) // Updates the position of the segments from the back
+                    {
+                        int t = snakeBody.Count();
+                        snakeBody[t].posX = snakeBody[t - 1].posX;
+                        snakeBody[t].posY = snakeBody[t - 1].posY;
+                    }
+
+                    Render();
+                }
+                
+
+                
+            }
             
 
-            Render();
+            
             Console.ReadKey();
 
 
+        }
+    }
+
+    public class GameObject
+    {
+        public int posX;
+        public int posY;
+    }
+
+    public class SnakeSegment: GameObject
+    {
+
+        
+
+        public SnakeSegment(int x, int y)
+        {
+            posX = x;
+            posY = y;
+        }
+
+    }
+
+    public class SnakeHead: GameObject
+    {
+        
+
+        public SnakeHead(int x, int y)
+        {
+            posX = x;
+            posY = y;
         }
     }
 }
